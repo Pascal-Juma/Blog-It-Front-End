fba36e51e27085d6a24beb98602a233122389f53
Pascal-Juma <pascaljuma2002@gmail.com>
2025-04-10 16:19:50 +0300

    Build the header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,20 @@
+import Button, { ButtonProps} from '@mui/material/Button';
+import { styled }from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
+import { FaLongArrowAltRight } from "react-icons/fa";
 import './Header.css'
 import logo from '../../assets/logo.png'
+
+const HeaderCta = styled(Button)<ButtonProps>(({ theme }) => ({
+  fontSize: '1.5rem',
+  fontFamily: 'var(--primary-font)',
+  fontWeight: 400,
+  color: theme.palette.getContrastText(blue[500]),
+  backgroundColor: blue[500],
+  '&hover': {
+    backgroundColor: blue[700],
+  }
+}));
 
 function Header() {
   return (
@@ -14,8 +29,9 @@ function Header() {
                 <li><a href="#" >About Us</a></li>
                 <li><a href="#" >Testimonials</a></li>
                 <li><a href="#" >Sign In</a></li>
-                <li><a href="#" >Get Started</a></li>
             </ul>
+            <HeaderCta variant="contained"  component="a"  href="#" sx={{ textTransform:  'capitalize'}}>Get Started <FaLongArrowAltRight />
+            </HeaderCta>
         </div>
       </nav>
     </>
